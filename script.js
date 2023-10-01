62357d0efb83e81c32d5a4468cffe816c4497c83
Grincode <juanpetarcich@gmail.com>
2023-10-01 13:42:57 -0300

    CV Completo con menu responsive

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const navToggler = document.querySelector(".nav-toggler");
+const navbar = document.querySelector(".navbar");
 
 navToggler.addEventListener("click", navToggle);
 
 function navToggle() {
-    console.log("hi");
-
+    navToggler.classList.toggle("active");
+    navbar.classList.toggle("open");
+    if (navbar.classList.contains("open")) {
+        navbar.style.maxHeight = navbar.scrollHeight + "px";
+    }
+    else {
+        navbar.removeAttribute("style");
+    }
 }
